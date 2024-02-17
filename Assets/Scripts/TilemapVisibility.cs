22a59b3397a2a83ae495759d803dc29c078ec2a4
John Patrick Quintos <113108715+patqnts@users.noreply.github.com>
2024-02-17 16:44:03 +0800

    CameraTrick

---
+++ Assets/Scripts/TilemapVisibility.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilemapVisibility : MonoBehaviour
+{
+    public float transitionSpeed = 0.2f;
+    private List<TilemapRenderer> tilemapRenderers;
+    private bool isPlayerInside = false;
+
+    void Start()
+    {
+        // Get all TilemapRenderer components under the Room object
+        tilemapRenderers = new List<TilemapRenderer>(GetComponentsInChildren<TilemapRenderer>());
+
+        // Set the initial transparency to fully transparent
+        SetTilemapTransparency(0f);
+    }
+
+    void Update()
+    {
+        // If the player is inside the trigger box, gradually reveal the tilemap
+        if (isPlayerInside)
+        {
+            SetTilemapTransparency(Mathf.Lerp(GetTilemapTransparency(), 1f, Time.deltaTime * transitionSpeed));
+        }
+        // If the player is outside the trigger box, gradually hide the tilemap
+        else
+        {
+            SetTilemapTransparency(Mathf.Lerp(GetTilemapTransparency(), 0f, Time.deltaTime * transitionSpeed));
+        }
+    }
+
+    // Triggered when another collider enters the trigger collider
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Vision"))
+        {
+            isPlayerInside = true;
+        }
+    }
+
+    // Triggered when another collider exits the trigger collider
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Vision"))
+        {
+            isPlayerInside = false;
+        }
+    }
+
+    // Set the transparency of all TilemapRenderers in the list
+    void SetTilemapTransparency(float alpha)
+    {
+        foreach (TilemapRenderer renderer in tilemapRenderers)
+        {
+            Color color = renderer.material.color;
+            color.a = alpha;
+            renderer.material.color = color;
+        }
+    }
+
+    // Get the transparency of the first TilemapRenderer in the list (assuming they have the same material)
+    float GetTilemapTransparency()
+    {
+        if (tilemapRenderers.Count > 0)
+        {
+            return tilemapRenderers[0].material.color.a;
+        }
+        return 0f;
+    }
+}

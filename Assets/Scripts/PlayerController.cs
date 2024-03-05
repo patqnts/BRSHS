0d636c1ef7930b9640c0196f66a624b25bd3e475
John Patrick Quintos <113108715+patqnts@users.noreply.github.com>
2024-03-05 21:33:21 +0800

    character update with attack

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,7 +10,7 @@ public class PlayerController : MonoBehaviour
     private Animator animator;
     public CircleCollider2D circleCollider;
     private Rigidbody2D rb;
-
+    private bool isAttacking = false;
     private void Start()
     {
         joystick = FindObjectOfType<FixedJoystick>();
@@ -20,7 +20,15 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        movementInput = new Vector2(joystick.Horizontal, joystick.Vertical);
+      
+       movementInput = new Vector2(joystick.Horizontal, joystick.Vertical);
+        
+        
+
+        if (Input.GetKeyDown(KeyCode.Space) && !isAttacking)
+        {
+            StartCoroutine(Attack());
+        }
     }
 
     private void FixedUpdate()
@@ -28,14 +36,31 @@ public class PlayerController : MonoBehaviour
         MovePlayer();
     }
 
+
     void MovePlayer()
     {
-        Vector2 movement = new Vector2(movementInput.x, movementInput.y);
-        movement.Normalize();
+        if (!isAttacking)
+        {
+            Vector2 movement = new Vector2(movementInput.x, movementInput.y);
+            movement.Normalize();
 
-        rb.velocity = movement * moveSpeed;
+            rb.velocity = movement * moveSpeed;
 
-        UpdateAnimatorParameters(movement);
+            UpdateAnimatorParameters(movement);
+        }
+        else
+        {
+            // Stop the player's movement while attacking
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+    public void AttackButton()
+    {
+        if(!isAttacking)
+        {
+            Attack();
+        }
     }
 
     private void UpdateAnimatorParameters(Vector2 movement)
@@ -54,5 +79,36 @@ public class PlayerController : MonoBehaviour
         circleCollider.enabled = true;
         yield return new WaitForSeconds(1f);
         circleCollider.enabled = false;
+    }
+
+    private IEnumerator Attack()
+    {
+        isAttacking = true;
+
+        Vector2 attackDirection = new Vector2(animator.GetFloat("x"), animator.GetFloat("y"));
+
+        // Determine the direction of the attack and play the corresponding animation
+        if (Mathf.Abs(attackDirection.x) > Mathf.Abs(attackDirection.y))
+        {
+            // Horizontal attack
+            if (attackDirection.x > 0)
+                animator.Play("attack right");
+            else
+                animator.Play("attack left");
+        }
+        else
+        {
+            // Vertical attack
+            if (attackDirection.y > 0)
+                animator.Play("attack up");
+            else
+                animator.Play("attack down");
+        }
+
+        // Add a delay to sync with the attack animation
+        yield return new WaitForSeconds(0.4f);
+
+        isAttacking = false;
+        animator.Play("Movement");
     }
 }
